e857e9130788d767097bc5dd8726d59365c1c5e5
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-09-07 17:48:53 -0300

    Added Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+
+  return (
+    <header id="main">
+      <div className="header-heading">
+        <h2>STEP UP YOUR</h2>
+        <h1><span>FITNESS</span> WITH US</h1>
+        <p className="details"> Build You Body And Fitness With Professional Touch</p>
+        <div className="header-btns">
+          <a href="#" className="header-btn">JOIN US</a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,36 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from '../images/logo.png';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 
 
 function Navbar() {
-  const[nav,setnav] = useState(false);
+  const [nav, setnav] = useState(false);
 
-  const changeBackground = ()=>{
-    if (window.scrollY >= 50){
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
       setnav(true);
-    }else{
+    } else {
       setnav(false);
     }
   }
   window.addEventListener('scroll', changeBackground);
 
   return (
-    <nav className={nav ? 'nav active':'nav'}>
+    <nav className={nav ? 'nav active' : 'nav'}>
       <Link to="#" className='logo'>
         <img src={logo} alt="Gym logo" />
       </Link>
-        <input className='menu-btn' type="checkbox" id="menu-btn" />
-        <label className='menu-icon' for='menu-btn'>
-          <span className='nav-icon'></span>
-        </label>
-        <ul className='menu'>
-          <li><Link to='#'>Header</Link></li>
-          <li><Link to='#'>Features</Link></li>
-          <li><Link to='#'>Offer</Link></li>
-          <li><Link to='#'>About</Link></li>
-          <li><Link to='#'>Contact</Link></li>
-        </ul>
+      <input className='menu-btn' type="checkbox" id="menu-btn" />
+      <label className='menu-icon' for='menu-btn'>
+        <span className='nav-icon'></span>
+      </label>
+      <ul className='menu'>
+        <li><Link to='#'>Header</Link></li>
+        <li><Link to='#'>Features</Link></li>
+        <li><Link to='#'>Offer</Link></li>
+        <li><Link to='#'>About</Link></li>
+        <li><Link to='#'>Contact</Link></li>
+      </ul>
     </nav>
   );
 }
